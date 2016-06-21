a2b136ae9e7bf3eb333bf3c45f126c3f509ac517
stephen <stephenranjit@gmail.com>
2016-06-21 18:52:58 +0530

    simple script to setup kubernetes-dashboard

--- setup_dashboard.sh
+++ setup_dashboard.sh
@@ -18,9 +18,22 @@ if [ $r == 0 ]; then
 else
     echo "no kubelet found"
 fi
+
 # setup kubelet on the master
+echo "please setup kubelet manually in order to use kubernetes-dashboard"
 
 # setup namespace
 
+if kubectl create -f https://raw.githubusercontent.com/stephenranjit/kubernetes-dashboard/master/kube-system.yaml; then
+	echo "something went wrong while installing the namespace kube-system"
+else
+	echo "successfully installed namespace kube-system"
+fi
 
 # install kubernetes-dashboard
+
+if kubectl create -f https://raw.githubusercontent.com/stephenranjit/kubernetes-dashboard/master/kubernetes-dashboard.yaml; then
+	echo "something went wrong while installing the kube-dashboard"
+else
+	echo "successfully installed namespace kube-dashboard"
+fi
